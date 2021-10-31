fe82641915cf84422ff0f995c772ffdfe1a6634e
Alexander <alexandrosliberalis@gmail.com>
2021-10-31 20:35:17 +0200

    Run cleaning up.

--- Source/rendering_application.cpp
+++ Source/rendering_application.cpp
@@ -17,14 +17,8 @@ RenderingApplication::~RenderingApplication()
 
 void RenderingApplication::Run()
 {
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
+    InitializeWindow();
+    InitializeVulkan();
 
     while(!glfwWindowShouldClose(mWindow)) 
     {
