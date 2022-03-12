70f49e413dcb2c2e62b91b7f06fb1d44acb61896
Alexandr Obzherin <alexandrosliberalis@gmail.com>
2022-03-12 21:59:11 +0200

    Fill build instruction.

--- windows_compile_shaders.bat
+++ windows_compile_shaders.bat
@@ -9,7 +9,7 @@ cd Shaders
 d:
 
 echo "Shaders compilation..."
- VulkanSDK\1.2.189.2\Bin\glslc.exe E:\Development\VulkanProject\vulkanproject\Shaders\basic_shader.vert -o E:\Development\VulkanProject\vulkanproject\Build\Intermediate\Shaders\basic_shader_vert.spv
- VulkanSDK\1.2.189.2\Bin\glslc.exe E:\Development\VulkanProject\vulkanproject\Shaders\basic_shader.frag -o E:\Development\VulkanProject\vulkanproject\Build\Intermediate\Shaders\basic_shader_frag.spv
+ VulkanSDK\1.2.189.2\Bin\glslc.exe E:\Development\VulkanProject\rendering-engine\Shaders\basic_shader.vert -o E:\Development\VulkanProject\rendering-engine\Build\Intermediate\Shaders\basic_shader_vert.spv
+ VulkanSDK\1.2.189.2\Bin\glslc.exe E:\Development\VulkanProject\rendering-engine\Shaders\basic_shader.frag -o E:\Development\VulkanProject\rendering-engine\Build\Intermediate\Shaders\basic_shader_frag.spv
 
 
