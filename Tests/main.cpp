e97b2c96640e0947c030a73f750826418c5ea0be
Alexandr Obzherin <alexandrosliberalis@gmail.com>
2022-05-15 13:18:19 +0300

    CMake refactoring. GTests integration.

---
+++ Tests/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "gtest/gtest.h"
+
+using ::testing::EmptyTestEventListener;
+using ::testing::InitGoogleTest;
+using ::testing::Test;
+using ::testing::TestEventListeners;
+using ::testing::TestInfo;
+using ::testing::TestPartResult;
+using ::testing::TestSuite;
+using ::testing::UnitTest;
+
+TEST(InitialTest, InitialTest01) 
+{
+  EXPECT_EQ(0, 0);
+}
+
+int main(int argc, char** argv) 
+{
+    InitGoogleTest(&argc, argv);
+    UnitTest& unit_test = *UnitTest::GetInstance();
+
+    return RUN_ALL_TESTS();;
+}
