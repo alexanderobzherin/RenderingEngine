28ee05c58e9dca2d94234412422838878d298076
Alexandr Obzherin <alexandrosliberalis@gmail.com>
2025-06-25 14:36:43 +0300

    Rework README part 1

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,9 @@
+
+::Clean previously created binaries
 rmdir /s /q Build
-
+::Create new build directory and step into it
 mkdir Build
-
 cd Build
-
-cmake .. -DCMAKE_TOOLCHAIN_FILE=D:/vcpkg/scripts/buildsystems/vcpkg.cmake
+::Run root cmake file
+set TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE% -A x64
